6afc61d0f64136befdfd0640daca2b7c709c6256
thsc447 <thiago.campos@mercadolivre.com>
2022-01-27 14:19:44 -0300

    feat: add applitation_demo

--- src/main/java/br/com/meliw4/projetointegrador/entity/Setor.java
+++ src/main/java/br/com/meliw4/projetointegrador/entity/Setor.java
@@ -1,4 +1,28 @@
 package br.com.meliw4.projetointegrador.entity;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Setor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idSetor;
+    @NotEmpty(message = "A Categoria não pode estar vazia")
+    @NotNull()
+    private String categoria;
+    @NotEmpty(message = "O volume não pode estar vazia")
+    @NotNull()
+    private Double volume;
+
 }
---
+++ src/main/java/br/com/meliw4/projetointegrador/repository/SetorRepository.java
@@ -0,0 +1,7 @@
+package br.com.meliw4.projetointegrador.repository;
+
+import br.com.meliw4.projetointegrador.entity.Setor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetorRepository extends JpaRepository<Setor, Integer> {
+}
