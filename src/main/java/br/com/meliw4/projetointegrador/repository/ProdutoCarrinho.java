d8b8c020494fc2a032b617f670e6c479b10e3ce1
Marcelo Scarton <marcelo.scarton@mercadolivre.com>
2022-01-31 17:31:18 -0300

    Add pedido request

--- src/main/java/br/com/meliw4/projetointegrador/repository/ProdutoCarrinho.java
+++
@@ -1,7 +0,0 @@
-package br.com.meliw4.projetointegrador.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoCarrinho extends JpaRepository<ProdutoCarrinho, Long> {
-}
