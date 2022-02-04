1afc13d43137bbf0fe8f0c966d7827b673515742
thsc447 <thiago.campos@mercadolivre.com>
2022-02-04 11:49:18 -0300

    permitindo acesso ao h2

--- src/main/java/br/com/meliw4/projetointegrador/config/SecurityConfig.java
+++ src/main/java/br/com/meliw4/projetointegrador/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package br.com.meliw4.projetointegrador.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,9 +12,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
 	public static final String[] PUBLIC_MATCHES = {
 		"/h2-console/**",
 		"/api/v1/**"
@@ -20,6 +28,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("teste"))
+			http.headers().frameOptions().disable();
+
 		http.cors().and().csrf().disable();
 		http.authorizeHttpRequests()
 			.antMatchers(PUBLIC_MATCHES)
