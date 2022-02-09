7331e3410da33bb0ce156f48c3ac8504eedfe149
André Arroxellas <andre.arroxellas@mercadolivre.com>
2022-02-09 17:19:31 -0300

    save

--- src/main/java/br/com/meliw4/projetointegrador/ProjetoIntegradorApplication.java
+++ src/main/java/br/com/meliw4/projetointegrador/ProjetoIntegradorApplication.java
@@ -3,11 +3,14 @@ package br.com.meliw4.projetointegrador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.meliw4.projetointegrador.service.Dummy1;
+
 @SpringBootApplication
 public class ProjetoIntegradorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjetoIntegradorApplication.class, args);
-    }
+	public static void main(String[] args) {
+		// SpringApplication.run(ProjetoIntegradorApplication.class, args);
+		Dummy1.connect();
+	}
 
 }
