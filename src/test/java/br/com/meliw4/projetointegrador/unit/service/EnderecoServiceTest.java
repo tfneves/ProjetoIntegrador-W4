1e01294b2fc9cacebec037ad9bab0f1c50fcd896
Marcelo Scarton <marcelo.scarton@mercadolivre.com>
2022-02-11 09:15:26 -0300

    Fix test

--- src/test/java/br/com/meliw4/projetointegrador/unit/service/EnderecoServiceTest.java
+++ src/test/java/br/com/meliw4/projetointegrador/unit/service/EnderecoServiceTest.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import static org.mockito.Mockito.*;
 
+import br.com.meliw4.projetointegrador.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
 
 import br.com.meliw4.projetointegrador.dto.EnderecoDTO;
 import br.com.meliw4.projetointegrador.entity.Endereco;
 import br.com.meliw4.projetointegrador.repository.EnderecoRepository;
 import br.com.meliw4.projetointegrador.service.EnderecoService;
+import org.mockito.Mockito;
+
+import java.util.Optional;
 
 public class EnderecoServiceTest {
 	private static EnderecoRepository enderecoRepository = mock(EnderecoRepository.class);
@@ -17,21 +21,16 @@ public class EnderecoServiceTest {
 
 	@Test
 	void shouldGetById() {
-		when(enderecoRepository.getById(1L)).thenReturn(Endereco.builder().id(1L).build());
+		Mockito.when(enderecoRepository.findById(1L)).thenReturn(Optional.of((Endereco) Endereco.builder().id(1L).build()));
 
 		Endereco response = enderecoService.getById(1L);
 		assertEquals(1L, response.getId());
 	}
 
-	void shouldNotThrowExceptionWhenGetById() {
-		assertDoesNotThrow(
-				() -> enderecoService.getById(any()));
-	}
-
 	@Test
 	void testRegister() {
 		EnderecoDTO enderecoDTO = EnderecoDTO.builder().logradouro("Logradouro 1").numero(1).bairro("bairro")
-				.cidade("cidade").estado("estado").build();
+			.cidade("cidade").estado("estado").build();
 
 		Endereco response = enderecoService.register(enderecoDTO);
 		assertEquals(1, response.getNumero());
